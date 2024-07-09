fab7565a904a8e0382c18607fd21c19b67fb05cb
Lukas Trumm <lukas.trumm@gmail.com>
2024-07-09 10:29:10 +0200

    Add scripts, pin tool versions, format code, add license

--- src/module.ts
+++ src/module.ts
@@ -1,12 +1,12 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver } from "@nuxt/kit"
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: "my-module",
+    configKey: "myModule",
   },
   // Default configuration options of the Nuxt module
   defaults: {},
@@ -14,6 +14,6 @@ export default defineNuxtModule<ModuleOptions>({
     const resolver = createResolver(import.meta.url)
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    addPlugin(resolver.resolve("./runtime/plugin"))
   },
 })
